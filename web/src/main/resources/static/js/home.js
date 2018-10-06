c5356ddc1886426796c0b5e8d37da4b85f7eb7c9
pwxc <1050270321@qq.com>
2018-10-06 17:57:58 +0800

    refactor: js files

---
+++ web/src/main/resources/static/js/home.js
@@ -0,0 +1,58 @@
+const loginName = $('#session-name').text();
+
+const repos = new Vue({
+    el: '#repos',
+    data: {
+        repositories: [],
+    },
+    methods: {
+        searchRepository: function () {
+            fetch(`/api/repository/${loginName}`)
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res);
+                    this.repositories = res;
+                });
+        },
+    }
+});
+repos.searchRepository();
+
+const subs = new Vue({
+    el: '#subs',
+    data: {
+        subscriptions: [],
+    },
+    methods: {
+        moment: function () {
+            return moment();
+        },
+        searchSubscriptions: function () {
+            fetch(`/api/userSubscription/${loginName}`)
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res);
+                    this.subscriptions = res;
+                });
+        },
+    }
+});
+subs.searchSubscriptions()
+
+const recommends = new Vue({
+    el: '#recommends',
+    data: {
+        recommends: [],
+    },
+    methods: {
+        searchRepository: function () {
+            fetch(`/api/random/repository/`)
+                .then(res => res.json())
+                .then(res => {
+                    console.log(res);
+                    this.recommends = res;
+                });
+        },
+    }
+});
+recommends.searchRepository();
